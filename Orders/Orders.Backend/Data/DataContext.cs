010b2cf6d1c217dbab56a8c6e87fbfbe630a30ac
kyo kusanagui <nvll@hotmail.com>
2025-09-19 13:05:59 -0600

    Complete generic controller

--- Orders/Orders.Backend/Data/DataContext.cs
+++ Orders/Orders.Backend/Data/DataContext.cs
@@ -9,12 +9,13 @@ public class DataContext : DbContext
     {
     }
 
+    public DbSet<Category> Categories { get; set; }
     public DbSet<Country> Countries { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
-        modelBuilder.Entity<Country>().HasIndex(x => x.Name).IsUnique(
-        );
+        modelBuilder.Entity<Country>().HasIndex(x => x.Name).IsUnique();
+        modelBuilder.Entity<Category>().HasIndex(x => x.Name).IsUnique();
     }
 }
